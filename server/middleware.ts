bbe700fc29ecb2c1454ea59ec7a5506fe80325ef
maddsua <maddsuadev@proton.me>
2023-11-29 23:55:58 +0200

    Update middleware.ts

--- server/middleware.ts
+++ server/middleware.ts
@@ -1,8 +1,42 @@
+import { Context, findAllRoutes, loadRoutes } from "./routes.ts";
+import { JSONResponse } from "./api.ts";
 
-import type { RouteHandler } from "./api.ts";
+interface OctopussOptions {
+	routesDir: string;
+};
 
-export class OctopussLiteMiddleware {
+interface StartServerOptions {
+	serve?: Deno.ServeOptions | Deno.ServeTlsOptions;
+	octo?: OctopussOptions;
+};
 
-	//handlers: Record<string, ServiceHandler>;
+export const startServer = async (opts?: StartServerOptions) => {
 
+	let searchDir = opts?.octo?.routesDir || 'src/routes';
+
+	const handlers = await findAllRoutes(searchDir);
+	if (!handlers.entries.length) throw new Error(`Failed to load route modules: no modules found in "${searchDir}"`);
+
+	const routes = await loadRoutes(handlers);
+
+	const middlewareHandler: Deno.ServeHandler = async (request, info) => {
+
+		const { pathname } = new URL(request.url);
+
+		const route = routes[pathname];
+		if (!route) {
+			return new Response(null, {
+				status: 404
+			});
+		}
+	
+		const handlerResponse = await route.handler(request, {} as Context);
+		
+		return handlerResponse instanceof JSONResponse ? new Response(handlerResponse.body, {
+			headers: handlerResponse.headers,
+			status: handlerResponse.status
+		}) : handlerResponse;
+	};
+
+	Deno.serve(opts?.serve || {}, middlewareHandler);
 };
