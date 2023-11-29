280c9f14d5e8ca1eb62fb92811c529e3a777f76c
maddsua <maddsuadev@proton.me>
2023-11-30 01:47:43 +0200

    Update originManager.ts

--- server/originManager.ts
+++ server/originManager.ts
@@ -7,3 +7,39 @@ export interface OriginManagerConfig {
 export interface OriginManagerOptions extends Partial<OriginManagerConfig> {
 	enabled: boolean;
 };
+
+export class OriginChecker {
+
+	allowedOrigins: string[];
+
+	constructor(origins: string[]) {
+		this.allowedOrigins = origins;
+	}
+
+	check(rqOrigin: string) {
+
+		if (!rqOrigin) return false;
+
+		let hostnameStart = rqOrigin.indexOf("://");
+		if (hostnameStart === -1) return false;
+		hostnameStart += 3;
+		
+		const portStart = rqOrigin.indexOf(':', hostnameStart);
+		const pathStart = rqOrigin.indexOf('/', hostnameStart);
+		
+		let hostnameEnd = undefined;
+
+		if (portStart !== -1) {
+			hostnameEnd = portStart;
+		} else if (pathStart !== -1) {
+			hostnameEnd = pathStart;
+		}
+
+		const originHostname = rqOrigin.slice(hostnameStart, hostnameEnd);
+
+		return this.allowedOrigins.some(domain => (
+			originHostname === domain ||
+			originHostname.endsWith(`.${domain}`)
+		));
+	}
+};
