97e5998bc1b8d5e01e32bedc2277f27be29ba98f
maddsua <maddsuadev@proton.me>
2023-12-03 21:37:25 +0200

    Update middleware.ts

--- server/middleware.ts
+++ server/middleware.ts
@@ -33,13 +33,21 @@ export const startServer = async (opts?: StartServerOptions) => {
 
 	const httpRequestHandler: Deno.ServeHandler = async (request, info) => {
 
-		const requestID = (opts?.octo?.proxy?.requestIdHeader ?
-			request.headers.get(opts.octo.proxy.requestIdHeader) : undefined) ||
-			Array.apply(null, Array(8)).map(() => {
-				const characters = 'abcdefghijklmnopqrstuvwxyz0123456789'
-				return characters.charAt(Math.floor(Math.random() * characters.length))
-			}).join('');
+		const getRequestIdFromProxy = (headerName: string | null | undefined) => {
+			if (!headerName) return undefined;
+			const header = request.headers.get(headerName);
+			if (!header) return undefined;
+			const shortid = header.slice(0, header.indexOf('-'));
+			return shortid.length <= 8 ? shortid : shortid.slice(0, 8);
+		};
 
+		const generateRequestId = () => {
+			const characters = 'abcdefghijklmnopqrstuvwxyz0123456789';
+			const randomChar = () => characters.charAt(Math.floor(Math.random() * characters.length));
+			return Array.apply(null, Array(8)).map(randomChar).join('');
+		};
+
+		const requestID = getRequestIdFromProxy(opts?.octo?.proxy?.requestIdHeader) || generateRequestId();
 		const requestIP = (opts?.octo?.proxy?.forwardedIPHeader ?
 			request.headers.get(opts.octo.proxy.forwardedIPHeader) : undefined) ||
 			info.remoteAddr.hostname;
