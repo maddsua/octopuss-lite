770041975ac9c94c322d5d8b261686f0303731f1
maddsua <maddsuadev@proton.me>
2023-12-03 15:50:02 +0200

    Update bootstrap.ts

--- server/bootstrap.ts
+++ server/bootstrap.ts
@@ -1,8 +1,18 @@
 import { startServer } from "./middleware.ts";
 
+const smartParseInt = (variable: string | null | undefined): number | undefined => {
+	if (typeof variable !== 'number') return undefined;
+	const numval = parseInt(variable);
+	if (isNaN(numval)) return undefined;
+	return numval;
+};
+
 await startServer({
 	serve: {
-		port: 8080
+		port: smartParseInt(Deno.env.get('PORT')) || 8080,
+		hostname: Deno.env.get('HOSTNAME'),
+		cert: Deno.env.get('TLS_CERT'),
+		key: Deno.env.get('TLS_KEY')
 	},
 	octo: {
 		routesDir: 'src/routes'
