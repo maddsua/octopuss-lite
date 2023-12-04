e6a645c4e24d8efdebca477307c2bd14e15ccea9
maddsua <maddsuadev@proton.me>
2023-12-04 07:46:04 +0200

    Update bootstrap.ts

--- server/bootstrap.ts
+++ server/bootstrap.ts
@@ -1,15 +1,9 @@
 import { startServer } from "./middleware.ts";
-
-const smartParseInt = (variable: string | null | undefined): number | undefined => {
-	if (typeof variable !== 'number') return undefined;
-	const numval = parseInt(variable);
-	if (isNaN(numval)) return undefined;
-	return numval;
-};
+import { getNumber } from "./envutils.ts";
 
 await startServer({
 	serve: {
-		port: smartParseInt(Deno.env.get('OCTO_PORT') || Deno.env.get('PORT')) || 8080,
+		port: getNumber(Deno.env.get('OCTO_PORT') || Deno.env.get('PORT')),
 		hostname: Deno.env.get('OCTO_HOSTNAME'),
 		cert: Deno.env.get('OCTO_TLS_CERT'),
 		key: Deno.env.get('OCTO_TLS_KEY')
@@ -19,8 +13,8 @@ await startServer({
 		handleCORS: Deno.env.get('OCTO_HANDLE_CORS') !== 'false',
 		allowedOrigings: Deno.env.get('OCTO_ALLOWED_ORIGINS')?.split(',').map(item => item.trim()),
 		rateLimit: {
-			period: smartParseInt(Deno.env.get('OCTO_RATELIMIT_PERIOD')),
-			requests: smartParseInt(Deno.env.get('OCTO_RATELIMIT_REQUESTS')),
+			period: getNumber(Deno.env.get('OCTO_RATELIMIT_PERIOD')),
+			requests: getNumber(Deno.env.get('OCTO_RATELIMIT_REQUESTS')),
 		},
 		exposeRequestID: Deno.env.get('OCTO_EXPOSE_REQUEST_ID') !== 'false',
 	}
