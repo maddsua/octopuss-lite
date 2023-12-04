6447eba3e94882a1c8f910c4ee3a354318b94189
maddsua <maddsuadev@proton.me>
2023-12-04 07:42:34 +0200

    Update envutils.ts

--- server/envutils.ts
+++ server/envutils.ts
@@ -1,17 +1,17 @@
 
-export const getNumber = (envVar: string | null | undefined, fallback?: number) => {
+export const getNumber = (envVar: string | null | undefined, fallback?: number): number | undefined => {
 	if (!envVar?.length) return typeof fallback === 'number' ? fallback : undefined;
 	const temp = parseFloat(envVar);
 	if (isNaN(temp)) return typeof fallback === 'number' ? fallback : undefined;
 	return temp;
 };
 
-export const getBool = (envVar: string | null | undefined, fallback?: boolean) => {
-	if (!envVar?.length) return typeof fallback === 'boolean' ? fallback : false;
+export const getBool = (envVar: string | null | undefined, fallback?: boolean): boolean | undefined => {
+	if (!envVar?.length) return typeof fallback === 'boolean' ? fallback : undefined;
 	return envVar.toLowerCase().trim() === 'true';
 }
 
-export const getCommaSeparated = (envVar: string | null | undefined) =>  {
-	if (!envVar?.length) return [];
+export const getCommaSeparated = (envVar: string | null | undefined): string[] | undefined =>  {
+	if (!envVar?.length) return undefined;
 	return envVar.split(',').map(item => item.trim()).filter(item => !!item.length);
 };
