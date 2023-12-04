1a071f1da4998d7130c4d38a182509b59d9c2dcb
maddsua <maddsuadev@proton.me>
2023-12-04 07:39:09 +0200

    Update mod.ts

--- mod.ts
+++ mod.ts
@@ -1,10 +1,12 @@
 import type { RouteHandler, RouteConfig } from "./server/routeHandlers.ts";
 import { JSONResponse } from "./server/api.ts";
 import { startServer } from "./server/middleware.ts";
+import * as envutils from './server/envutils.ts';
 
 export {
 	RouteHandler,
 	RouteConfig,
 	JSONResponse,
 	startServer,
+	envutils,
 }
